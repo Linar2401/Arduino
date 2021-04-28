fb0ebaf47535f23075476eeb2140d4dbab9e6ce0
Linar2401 <89520Tes02598>
2021-04-28 11:33:07 +0300

    Alpha version

--- RobotBase4/RobotBase4.ino
+++ RobotBase4/RobotBase4.ino
@@ -2,10 +2,14 @@
 #include <Wire.h>
 #include <MPU6050.h>
 #include <GyverMotor.h>
+#include <Servo.h>
 
-#define Mot1 7
-#define Mot2 8
-#define Mot3 9
+#define ServoPin1 7
+#define ServoPin2 8
+
+#define Mot1 A2
+#define Mot2 A1
+#define Mot3 A0
 
 #define Mot4 4
 #define Mot5 5
@@ -41,6 +45,9 @@ int minSpeed[2] = {70, 110};
 GMotor motor1(DRIVER3WIRE, Mot1, Mot2, Mot3, LOW);
 GMotor motor2(DRIVER3WIRE, Mot4, Mot5, Mot6, LOW);
 
+Servo servo1;
+Servo servo2;
+
 void LWheelCounter(){
   lWheelCounter++;
 }
@@ -59,13 +66,17 @@ void turn(int angle){
   int point = yaw + angle;
   int err = point - yaw;
   while(abs(err) > 1.5){
-
-    motor1.setSpeed(minSpeed[0]*(err/abs(err)));
-    motor2.setSpeed(-minSpeed[1]*(err/abs(err)));
-
+    err = point - yaw;
+    Serial.println(err);
+    motor1.setSpeed((minSpeed[0] + abs(err) + 5)*(err/abs(err)));
+    motor2.setSpeed(-(minSpeed[1] + abs(err) + 5)*(err/abs(err)));
     Vector norm = mpu.readNormalizeGyro();
     yaw = yaw + norm.ZAxis * (millis()-timer)/1000;
     timer = millis();
+    delay(35);
+    motor1.setSpeed(0);
+    motor2.setSpeed(0);
+    delay(35);
   }
   motor1.setSpeed(0);
   motor2.setSpeed(0);
@@ -84,8 +95,14 @@ void move(int v, int l){
         err = point - yaw;
         speed = constrain(k_p*abs(err) + (abs(err) - abs(prev_err))*k_d, 0, MaxSpeed-MinSpeed - v);
                
-        motor1.setSpeed(minSpeed[0] + v + (speed)*(err/abs(err)));
-        motor2.setSpeed(minSpeed[1] + v -(speed)*(err/abs(err)));
+        if(v > 0){
+          motor1.setSpeed(minSpeed[0] + v + (speed)*(err/abs(err)));
+          motor2.setSpeed(minSpeed[1] + v -(speed)*(err/abs(err)));
+        }
+        else{
+          motor1.setSpeed(-(minSpeed[0] + v + (speed)*(err/abs(err))));
+          motor2.setSpeed(-(minSpeed[1] + v -(speed)*(err/abs(err))));
+        }
         Vector norm = mpu.readNormalizeGyro();
         yaw = yaw + norm.ZAxis * (millis()-timer)/1000;
         timer = millis();
@@ -96,6 +113,69 @@ void move(int v, int l){
 }
 
 
+void grab(bool s){
+  if(s){
+    servo1.write(90);
+    servo2.write(0);
+  }
+  else{
+    servo1.write(0);
+    servo2.write(90);
+  }
+  
+}
+
+int getDist(){
+  return 20;
+}
+
+
+void commandGet(){
+  int dist[5];
+  turn(-20);
+  for(int i = 0; i < 5; i++){
+    dist[i] = getDist();
+    if(i < 4){
+      turn(10);
+    }
+  }
+  turn(-20);
+  int min_ang = 0;
+  for(int i = 1; i < 5; i++){
+    if(dist[min_ang] > dist[i]){
+      min_ang = i;
+    }
+  }
+  turn(min_ang*10-20);
+  
+  move(20, dist[min_ang]*10 - 50);
+  grab(true);
+  move(-20, dist[min_ang]*10 - 50);
+  turn(-(min_ang*10-20));
+}
+
+void commandRelease(){
+  move(20, 100);
+  grab(false);
+  move(-20,100);
+}
+
+void commandGo2Position(int positionNumber){
+  move(-40, 200*(positionNumber/2));
+  turn(90*(positionNumber%2*2-1));
+}
+
+void commandGoFromPosition(int positionNumber){
+  turn(-90*(positionNumber%2*2-1));
+  move(40, 200*(positionNumber/2));
+}
+
+void commandGetPackage(int positionNumber){
+  commandGo2Position(positionNumber);
+  commandGet();
+  commandGoFromPosition(positionNumber);
+  commandRelease();
+}
 
 void setup() 
 {
@@ -112,12 +192,37 @@ void setup()
 
   mpu.calibrateGyro();
   mpu.setThreshold(3);
+
+  servo1.attach(ServoPin1);
+  servo2.attach(ServoPin2);
 }
+
+int value = -1;
+int state = -1;
+int data = -1;
 
 void loop()
 {
-  Serial.println("Start");
-  move(50, 500);
-  // motor2.setSpeed(110);
-  delay(1000000000);
+//  Serial.println("Start");
+   if (Serial.available() > 0)
+  {
+    data = Serial.parseInt();
+    if (data != 0)
+    {
+      state = data/1000;
+      value = data % 1000;
+      Serial.println(data);
+    }
+  }
+  switch (state)
+  {
+  case 1:
+    commandGetPackage(value);       
+    state = 0;
+    break;
+  default:
+    state = 0;
+    break;
+  }
+//  delay(1000000000);
 }
